317b80aca9cd6a039e6db1c10a5d43a77411f75a
Tamanna Sharma <tamanna.23bcon1021@jecrcu.edu.in>
2026-05-08 10:01:28 +0530

    normal

--- day8/abstration.cpp
+++ day8/abstration.cpp
@@ -1,34 +1,42 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-class atm{
-    private:
+class ATM
+{
+private:
     int balance;
-    public:
-    atm(int balance) {
-        this->balance=balance;
 
-    };
-    
-
-    void withdraw(int amt){
-        if(amt>balance){
-            cout<<"insufficent balance";
-
-        }
-        else{
-             balance-=amt;
-             cout<<"the remaining amt is: "<<amt<<endl;
-        }
-       
-
+public:
+    ATM(int balance)
+    {
+        this->balance = balance;
     }
-    void showbalance(){
-        cout<<"the current balance"<<balance<<endl;
+    // Withdraw Method of Atm
+    void Withdraw(int amount)
+    {
+        if (amount > balance)
+        {
+            cout << "Insufficient Balance" << endl;
+        }
+        else
+        {
+            balance -= amount;
+            cout << "Amount Withdrawn: " << amount << endl;
+        }
     }
-
+    // Show Balance Method of Atm
+    void ShowBalance()
+    {
+        cout << "Current Balance: " << balance << endl;
+    }
 };
-int main(){
-    atm a(5000);
-    a.withdraw(500);
-    a.showbalance();
+
+int main()
+{
+    ATM a(5000);
+    a.ShowBalance();
+    a.Withdraw(500);
+    a.ShowBalance();
+
+    return 0;
 }
